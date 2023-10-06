ad9f08364fb55d775124115990978cc319ee3c6e
mycampo1 <barriguibela@gmail.com>
2023-10-05 22:15:06 -0500

    calculadora creada en angular, solo es suma se ira mejorando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-app';
+  titulo = 'Aplicacion calculadora'
+  operandoA: number = 0;
+  operandoB: number = 0;
+  resultado: number = 0;
+
+
+  sumar():void{
+    this.resultado = this.operandoA + this.operandoB
+  }
 }
+
